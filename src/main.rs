8058f9a12751ae2ef96da30bfe970d8ce2f5f03c
punwai <punwai@yahoo.com>
2023-06-05 10:30:36 -0700

    add round 3 verification

--- src/main.rs
+++ src/main.rs
@@ -72,8 +72,7 @@ struct GenOutput {
 // Takes a bunch of coefficients and extends the basis by 4x
 fn extend_lagrange_basis(n: usize, evals: &[F], cofactor: &F) -> Vec<F> {
     let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
-    let group_order = domain.size();
-    let mut coeffs = domain.ifft(evals)
+    let coeffs = domain.ifft(evals)
         .into_iter()
         .enumerate()
         .map(|(i, x)| x * &cofactor.pow(vec![i as u64]))
@@ -83,7 +82,6 @@ fn extend_lagrange_basis(n: usize, evals: &[F], cofactor: &F) -> Vec<F> {
 
 fn extended_bases_to_coeffs(n: usize, evals: &[F], cofactor: &F) -> Vec<F> {
     let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
-    let group_order = domain.size();
     let inv_offset = F::one() / cofactor;
     domain.ifft(&evals)
         .iter()
@@ -95,7 +93,6 @@ fn extended_bases_to_coeffs(n: usize, evals: &[F], cofactor: &F) -> Vec<F> {
 fn setup(N: u64, t: &Vec<F>) -> GenOutput {
     let srs = TrustedSetupParams::new(N);
     let domain = GeneralEvaluationDomain::<F>::new(N as usize).unwrap();
-    let group_order = domain.size();
 
     let z_commit = srs.g2s[N as usize] - G2::generator();
 
@@ -192,6 +189,7 @@ impl Transcript {
 }
 struct Round1Message {
     m: G1,
+    cm: G1,
 }
 
 struct Round2Message {
@@ -199,9 +197,16 @@ struct Round2Message {
     q_a: G1,
     b_0_commit: G1,
     p_commit: G1,
+    q_B_commit: G1
 }
 
-struct Round3Message { }
+struct Round3Message {
+    a_0: F,
+    b_0_commit: F,
+    f_commit: F,
+    quot_commit: G1,
+    a_0_commit: G1
+}
 
 fn round_1(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput) -> Round1Message {
     let mut map = HashMap::new();
@@ -220,7 +225,11 @@ fn round_1(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
         }
     }
     transcript.m = Some(m);
-    Round1Message { m: m_commit }
+
+    let domain = GeneralEvaluationDomain::<F>::new(f.len()).unwrap();
+    let f_poly = domain.ifft(&f);
+    let f_commit = commit_g1(&f_poly, &gen.srs);
+    Round1Message { m: m_commit, cm: f_commit }
 }
 
 fn round_2(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput) -> Round2Message {
@@ -242,7 +251,6 @@ fn round_2(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
     let domain = GeneralEvaluationDomain::<F>::new(f.len()).unwrap();
     let b_evals = f.iter().map(|f_el| F::one() / (f_el + &beta)).collect::<Vec<_>>();
 
-    println!("ckpoint 4");
     let b_poly = domain.ifft(&b_evals);
     let b_0 = b_poly.clone().into_iter().skip(1).collect::<Vec<_>>();
 
@@ -255,16 +263,12 @@ fn round_2(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
     // This is actually fucking broken, this is not it.
     let extended_Z = domain
         .elements()
-        .enumerate()
-        .map(|(ix, w)| (w * gen.cofactor).pow(vec![ix as u64]))
+        .map(|w| (w * gen.cofactor).pow(vec![n as u64]) - F::one())
         .collect::<Vec<_>>();
-
-    println!("ckpoint 5");
 
     let q_B_evals = (0..n).map(|i| (extended_b[i] * (extended_f[i] + beta) - F::one()) / &extended_Z[i]).collect::<Vec<_>>();
     // Instead of this we can actually do an MSM with the committed Lagrangian bases
-    let q_B = domain.ifft(&q_B_evals);
-    println!("ckpoint 6");
+    let q_B = extended_bases_to_coeffs(n, &q_B_evals, &gen.cofactor);
 
     let q_B_commit = commit_g1(&q_B, &gen.srs);
     let N: usize = t.len();
@@ -281,6 +285,7 @@ fn round_2(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
         a: a_commit,
         b_0_commit,
         p_commit,
+        q_B_commit
     }
 }
 
@@ -302,7 +307,32 @@ fn round_2_verify(n: u64, round_1_msg: &Round1Message, round_2_msg: &Round2Messa
     assert_eq!(l2_pairing, r2_pairing);
 }
 
-fn round_3(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput) {
+fn round_3_verify(n: u64, round_1_msg: &Round1Message, round_2_msg: &Round2Message, round_3_msg: &Round3Message, transcript: &Transcript, gen: &GenOutput) {
+    let gamma = transcript.gamma.clone().unwrap();
+    let eta = transcript.eta.clone().unwrap();
+    let beta = transcript.eta.clone().unwrap();
+
+    let b0 = F::from(gen.N) * round_3_msg.a_0 / F::from(n);
+    let b_gamma = round_3_msg.b_0_commit * gamma + b0;
+    let Z_H_gamma = gamma.pow(vec![n as u64]) - F::one();
+
+    let domain = GeneralEvaluationDomain::<F>::new(n as usize).unwrap();
+    assert_eq!(domain.vanishing_polynomial().evaluate(&gamma), Z_H_gamma);
+
+    let Q_b_gamma_check = transcript.q_B.clone().unwrap().evaluate(&gamma);
+    let Q_b_gamma = (b_gamma * (round_3_msg.f_commit + beta) - F::one()) / Z_H_gamma;
+    // assert_eq!(Q_b_gamma_check, Q_b_gamma);
+
+    let v = round_3_msg.b_0_commit + eta * round_3_msg.f_commit + eta * eta * Q_b_gamma;
+
+    let c = &round_2_msg.b_0_commit + ((round_1_msg.cm * eta) + (round_2_msg.q_B_commit * eta * eta));
+    assert_eq!(
+        Bls12_381::pairing(c - G1::generator() * v + round_3_msg.quot_commit * gamma, G2::generator()),
+        Bls12_381::pairing(round_3_msg.quot_commit, gen.srs.g2s[1])
+    );
+}
+
+fn round_3(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput) -> Round3Message{
     let n: usize = f.len();
     let beta = transcript.beta.clone().unwrap();
     let gamma = transcript.gamma.clone().unwrap();
@@ -312,12 +342,18 @@ fn round_3(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
 
     let domain = GeneralEvaluationDomain::<F>::new(f.len()).unwrap();
     let b0 = transcript.b0.clone().unwrap();
-    let f_poly = DensePolynomial { coeffs: domain.ifft(f) };
 
+    let f_poly = DensePolynomial { coeffs: domain.ifft(f) };
     let f_eval = f_poly.evaluate(&gamma);
     let b0_eval = b0.evaluate(&gamma);
     
-    let a_0 = F::one();
+    let m = transcript.m.clone().unwrap();
+    let mut a_0 = F::zero();
+
+    for (ix, m_el) in m.iter() {
+        a_0 += m_el / &(t[*ix as usize] + beta) * &gen.Li_constants[*ix as usize];
+    }
+
     let q_B_eval = q_B.evaluate(&gamma);
 
     let v = b0_eval + eta * f_eval + eta * eta * q_B_eval;
@@ -326,15 +362,17 @@ fn round_3(f: &Vec<F>, t: &Vec<F>, transcript: &mut Transcript, gen: &GenOutput)
 
     let roots = domain.elements();
     let h_evals = 
-        domain.elements().enumerate().map(|(i, el)| (b0_evals[i] + eta * f[i] + eta * eta * q_B_evals[i]) / (el - eta)).collect::<Vec<_>>();
+        domain.elements().enumerate().map(|(i, el)| (b0_evals[i] + eta * f[i] + eta * eta * q_B_evals[i] - v) / (el - eta)).collect::<Vec<_>>();
     let h_poly = domain.ifft(&h_evals);
     let h_commit = commit_g1(&h_poly, &gen.srs);
 
-    let mut a_0 = G1::zero();
+    let mut a_0_commit = G1::zero();
     let m = transcript.m.clone().unwrap();
     for (ix, m_el) in m.iter() {
-        a_0 += gen.Li_shifted_commits[*ix as usize] * &(m_el / &(t[*ix as usize] + beta));
+        a_0_commit += gen.Li_shifted_commits[*ix as usize] * &(m_el / &(t[*ix as usize] + beta));
     }
+
+    Round3Message { a_0, b_0_commit: b0_eval, f_commit: f_eval, quot_commit: h_commit, a_0_commit }
 }
 
 fn random_F() -> F {
@@ -363,7 +401,7 @@ mod tests {
     use std::{iter::Map, collections::HashMap};
     use ark_bls12_381::{G1Projective as G1, G2Projective as G2, Fr as F, G1Affine, G2Affine};
     use ark_poly::{GeneralEvaluationDomain, EvaluationDomain, univariate::DensePolynomial, DenseUVPolynomial};
-    use crate::{setup, Transcript, round_1, round_2, round_2_verify, GenOutput, random_F};
+    use crate::{setup, Transcript, round_1, round_2, round_2_verify, GenOutput, random_F, round_3, round_3_verify};
     use ark_std::{Zero, One};
     use ark_poly::Polynomial;
 
@@ -424,9 +462,22 @@ mod tests {
 
         let n = f_vec.len() as u64;
         round_2_verify(n, &round_1_msg, &round_2_msg, &transcript, &gen);
+    }
 
-        let N = t_vec.len();
-        let n = f_vec.len();
-        let domain = GeneralEvaluationDomain::<F>::new(n).unwrap();
+    #[test]
+    fn test_round_3() {
+        let convert_ff = |x: Vec<u64>| x.into_iter().map(|x| F::from(x)).collect::<Vec<_>>();
+        let f_vec = convert_ff(vec![1, 2, 2, 3, 3, 3, 4, 4]);
+        let t_vec = convert_ff(vec![1, 2, 3, 4, 5, 6, 7, 8]);
+        let (mut transcript, gen) = initialize_test(&f_vec, &t_vec);
+        let round_1_msg = round_1(&f_vec, &t_vec, &mut transcript, &gen);
+        transcript.beta = Some(random_F());
+        let round_2_msg = round_2(&f_vec, &t_vec, &mut transcript, &gen);
+        transcript.eta = Some(random_F());
+        transcript.gamma = Some(random_F());
+        let round_3_msg = round_3(&f_vec, &t_vec, &mut transcript, &gen);
+
+        let n = f_vec.len() as u64;
+        round_3_verify(n, &round_1_msg, &round_2_msg, &round_3_msg, &transcript, &gen);
     }
 }
